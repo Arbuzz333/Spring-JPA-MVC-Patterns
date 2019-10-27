cca52b146bf613777ec4838705facb715cac9314
Arbuzz333 <arbuz333@hotmail.com>
2019-10-27 22:12:43 +0300

    MeatService with ordinal()

--- src/main/java/avakhidov/factories/bakery/BakeryCondition.java
+++ src/main/java/avakhidov/factories/bakery/BakeryCondition.java
@@ -2,11 +2,10 @@ package avakhidov.factories.bakery;
 
 import avakhidov.factories.entity.Product;
 import avakhidov.factories.entity.ingredient.Ingredient;
-import avakhidov.factories.service.Oven;
-import avakhidov.factories.market.Market;
+import avakhidov.factories.enums.BakeryConditionEnum;
 
 public interface BakeryCondition<T extends Product, S extends Ingredient> {
 
-    void update(Oven<T> oven, Market market, StorageBakery<S> bakery);
+    BakeryConditionEnum getCondition();
 
 }
--- src/main/java/avakhidov/factories/bakery/BakeryConditionBun.java
+++ src/main/java/avakhidov/factories/bakery/BakeryConditionBun.java
@@ -2,9 +2,12 @@ package avakhidov.factories.bakery;
 
 import avakhidov.factories.entity.bun.Bun;
 import avakhidov.factories.entity.flour.Flour;
+import avakhidov.factories.enums.BakeryConditionEnum;
 import avakhidov.factories.market.Market;
 import avakhidov.factories.service.Oven;
+import lombok.Setter;
 
+@Setter
 public class BakeryConditionBun implements BakeryCondition<Bun, Flour> {
 
     private Oven<Bun> oven;
@@ -13,8 +16,15 @@ public class BakeryConditionBun implements BakeryCondition<Bun, Flour> {
 
     private StorageBakery<Flour> storageBakery;
 
-    @Override
-    public void update(Oven<Bun> oven, Market market, StorageBakery<Flour> bakery) {
-
+    public BakeryConditionBun(Oven<Bun> oven, Market market, StorageBakery<Flour> bakery) {
+        this.oven = oven;
+        this.market = market;
+        this.storageBakery = bakery;
     }
+
+    @Override
+    public BakeryConditionEnum getCondition() {
+        return BakeryConditionEnum.DOWNTIME;
+    }
+
 }
