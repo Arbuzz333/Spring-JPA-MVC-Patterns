aaa1442552b6d36bc12304fc4063d494358e719b
Arbuzz333 <arbuz333@hotmail.com>
2020-01-05 18:35:10 +0300

    remove T flourDough;

--- src/main/java/avakhidov/factories/service/serviceimpl/CornBunRecipe.java
+++ src/main/java/avakhidov/factories/service/serviceimpl/CornBunRecipe.java
@@ -18,10 +18,10 @@ public class CornBunRecipe implements Recipe<Bun> {
     public Bun cooked(double weight) {
         ParameterPrepareDough<CornFlour> parameterPrepareDough =
                 ParameterDoughEnum.CORN_FLOUR_COARSE.toKneadTheDough();
-        parameterPrepareDough.setKindDoughAndFat(KindDough.PUFF_PASTRY, 3.5);
 
         CornBun cornBun = new CornBun(parameterPrepareDough, true, weight);
         cornBun.setFinished(Finished.RAW);
+        parameterPrepareDough.setKindDoughAndFat(KindDough.PUFF_PASTRY, 3.5);
         return cornBun;
     }
 }
