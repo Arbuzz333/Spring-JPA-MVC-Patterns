7f5aad21115e8aa8d6b675e4d53f0fc832fa509b
Arbuzz333 <arbuz333@hotmail.com>
2020-07-28 21:57:31 +0300

    add second factory for Flour

--- springpatterns/src/main/java/avakhidov/springpatterns/ApplicationRun.java
+++ springpatterns/src/main/java/avakhidov/springpatterns/ApplicationRun.java
@@ -4,14 +4,12 @@ package avakhidov.springpatterns;
 import avakhidov.anotations.EnableMainRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 
 
 
 
 @SpringBootApplication
-@PropertySource("classpath:value.properties")
 @EnableMainRunner
 public class ApplicationRun {
 
